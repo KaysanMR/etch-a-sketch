5efc797eb4d8892f6f4ba001ca8429eddd74e46e
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2023-05-07 07:40:40 +0500

    Sync input field to resolution slider

--- script.js
+++ script.js
@@ -2,17 +2,27 @@
 
 const grid = document.querySelector('#grid');
 const divWidth = grid.offsetWidth;
-let resolution = 16;
+let resolution = 2;
 
-function createGrid(res) {
-    grid.style.setProperty('--grid-rows', res);
-    grid.style.setProperty('--grid-cols', res);
-    for (i = 0; i < (res ** 2); i++) {
+
+grid.addEventListener("mousemove", draw);
+grid.addEventListener("dragstart", (e) => e.preventDefault());
+
+const slider = document.getElementById("gridSize");
+const field = document.querySelector(".inputNumber")
+
+updateResolution();
+
+createGrid(resolution);
+
+function createGrid() {
+    grid.style.setProperty('--grid-rows', resolution);
+    grid.style.setProperty('--grid-cols', resolution);
+    for (i = 0; i < (resolution ** 2); i++) {
         let cell = document.createElement("div");
         grid.appendChild(cell).className = `cell ${i}`;
     };
 };
-
 
 function draw(e) {
     e.stopPropagation();
@@ -21,15 +31,17 @@ function draw(e) {
     e.target.style.borderColor = "black"; 
 };
 
-grid.addEventListener("mousemove", draw);
-grid.addEventListener("dragstart", (e) => e.preventDefault());
+function updateResolution() {
+    slider.onchange = () => {
+        field.value = slider.value;
+        resolution = slider.value;
+        console.log(resolution);
+    };
 
-const size = document.getElementById("gridSize");
-
-size.oninput = () => {
-    resolution = size.value;
-    console.log(resolution);
+    field.onchange = () => {
+        slider.value = field.value;
+        resolution = field.value;
+        console.log(resolution);
+    };
 }
-
-createGrid(resolution);
 
